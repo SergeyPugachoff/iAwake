de54aef6a725594c5a0d7acd1b201d6c58b94387
Sergey <sergey.pugachoff@gmail.com>
2021-02-01 01:11:43 +0200

    - Implemented ui changes.

--- presentation/src/main/java/com/sergey/pugachov/iawake/ui/tracks/TracksFragment.kt
+++ presentation/src/main/java/com/sergey/pugachov/iawake/ui/tracks/TracksFragment.kt
@@ -2,14 +2,20 @@ package com.sergey.pugachov.iawake.ui.tracks
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnticipateOvershootInterpolator
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.isInvisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
 import coil.load
 import com.sergey.pugachov.iawake.R
 import com.sergey.pugachov.iawake.databinding.FragmentTracksBinding
 import com.sergey.pugachov.iawake.extentions.binding.viewBinding
+import com.sergey.pugachov.iawake.extentions.widgets.CheckableImageView
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -27,26 +33,74 @@ class TracksFragment : Fragment(R.layout.fragment_tracks) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.backButton.setOnClickListener {
-            findNavController().navigateUp()
+        binding.content.run {
+            backButton.setOnClickListener {
+                findNavController().navigateUp()
+            }
+
+            programCoverImage.load(arguments.programCoverUrl)
+            programTitle.text = arguments.programTitle
+            toolbarTitle.text = arguments.programTitle
+
+            playButton.setOnClickListener { button ->
+                button as CheckableImageView
+                if (button.isChecked) {
+                    viewModel.pause()
+                } else {
+                    viewModel.play()
+                }
+            }
+
+            tracksList.adapter = TracksAdapter(
+                onItemClick = { track ->
+                    viewModel.play(track)
+                }
+            )
+            tracksList.layoutManager = LinearLayoutManager(requireContext())
         }
 
-        binding.programCoverImage.load(arguments.programCoverUrl)
-        binding.programTitle.text = arguments.programTitle
-
-        binding.tracksList.adapter = TracksAdapter(
-            onPlayClicked = { track ->
-                viewModel.toggleTrackSelection(track)
+        binding.selectedTrackView.setOnClickListener { isPlayed ->
+            if (isPlayed) {
+                viewModel.pause()
+            } else {
+                viewModel.play()
             }
-        )
-        binding.tracksList.layoutManager = LinearLayoutManager(requireContext())
+        }
 
         viewModel.tracks.observe(viewLifecycleOwner, { tracks ->
-            (binding.tracksList.adapter as TracksAdapter).setItems(tracks)
+            (binding.content.tracksList.adapter as TracksAdapter).setItems(tracks)
         })
 
         viewModel.selectedTrack.observe(viewLifecycleOwner, { selectedTrack ->
-            (binding.tracksList.adapter as TracksAdapter).setSelectedTrack(selectedTrack)
+            binding.content.playButton.isChecked = selectedTrack.isPlaying()
+
+            (binding.content.tracksList.adapter as TracksAdapter).setSelectedTrack(selectedTrack)
+
+            binding.selectedTrackView.displayTrack(selectedTrack)
+            showSelectedTrackView()
         })
+    }
+
+    private fun showSelectedTrackView() {
+        if (binding.selectedTrackView.isInvisible) {
+            binding.selectedTrackView.isInvisible = false
+
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(binding.root)
+
+            val transition = ChangeBounds()
+            val interpolator = AnticipateOvershootInterpolator(1.0f)
+            transition.interpolator = interpolator
+
+            TransitionManager.beginDelayedTransition(binding.root, transition)
+            constraintSet.clear(binding.selectedTrackView.id, ConstraintSet.TOP)
+            constraintSet.connect(
+                binding.selectedTrackView.id,
+                ConstraintSet.BOTTOM,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.BOTTOM
+            )
+            constraintSet.applyTo(binding.root)
+        }
     }
 }
